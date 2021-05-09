08986b655708d6a1f28a31703003b0213a98f24c
Ludovic DEPLANQUE <ldeplanque58@gmail.com>
2021-05-09 15:46:13 +0200

    First commit

---
+++ PackCPC/Main.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace PackCPC {
+	partial class Main {
+		/// <summary>
+		/// Variable nécessaire au concepteur.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Nettoyage des ressources utilisées.
+		/// </summary>
+		/// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Code généré par le Concepteur Windows Form
+
+		/// <summary>
+		/// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+		/// le contenu de cette méthode avec l'éditeur de code.
+		/// </summary>
+		private void InitializeComponent() {
+			this.bpPack = new System.Windows.Forms.Button();
+			this.comboPackMethode = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.chkNoAmsdos = new System.Windows.Forms.CheckBox();
+			this.SuspendLayout();
+			// 
+			// bpPack
+			// 
+			this.bpPack.Location = new System.Drawing.Point(12, 33);
+			this.bpPack.Name = "bpPack";
+			this.bpPack.Size = new System.Drawing.Size(157, 23);
+			this.bpPack.TabIndex = 0;
+			this.bpPack.Text = "Open file to pack";
+			this.bpPack.UseVisualStyleBackColor = true;
+			this.bpPack.Click += new System.EventHandler(this.bpPack_Click);
+			// 
+			// comboPackMethode
+			// 
+			this.comboPackMethode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboPackMethode.FormattingEnabled = true;
+			this.comboPackMethode.Items.AddRange(new object[] {
+            "Standard",
+            "ZX0",
+            "ZX1"});
+			this.comboPackMethode.Location = new System.Drawing.Point(141, 6);
+			this.comboPackMethode.Name = "comboPackMethode";
+			this.comboPackMethode.Size = new System.Drawing.Size(90, 21);
+			this.comboPackMethode.TabIndex = 67;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(111, 13);
+			this.label1.TabIndex = 68;
+			this.label1.Text = "Compression method :";
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(6, 75);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(388, 134);
+			this.listBox1.TabIndex = 69;
+			// 
+			// chkNoAmsdos
+			// 
+			this.chkNoAmsdos.AutoSize = true;
+			this.chkNoAmsdos.Location = new System.Drawing.Point(224, 39);
+			this.chkNoAmsdos.Name = "chkNoAmsdos";
+			this.chkNoAmsdos.Size = new System.Drawing.Size(164, 17);
+			this.chkNoAmsdos.TabIndex = 70;
+			this.chkNoAmsdos.Text = "Save without Amsdos header";
+			this.chkNoAmsdos.UseVisualStyleBackColor = true;
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(400, 213);
+			this.Controls.Add(this.chkNoAmsdos);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboPackMethode);
+			this.Controls.Add(this.bpPack);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Main";
+			this.Text = "CPC packer files";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button bpPack;
+		private System.Windows.Forms.ComboBox comboPackMethode;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.CheckBox chkNoAmsdos;
+	}
+}
+
---
+++ PackCPC/Main.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace PackCPC {
+	public partial class Main : Form {
+		public enum PackMethode { None = 0, Standard, ZX0, ZX1 };
+
+		static private byte[] result = new byte[0x10000];
+
+		public Main() {
+			InitializeComponent();
+			comboPackMethode.SelectedItem = "Standard";
+		}
+
+		private void WriteInfo(string txt) {
+			listBox1.Items.Add(txt);
+			listBox1.SelectedIndex = listBox1.Items.Count - 1;
+		}
+
+		private void bpPack_Click(object sender, EventArgs e) {
+			OpenFileDialog dlgOpen = new OpenFileDialog();
+			dlgOpen.Filter = "All files (*.*)|*.*";
+			if (dlgOpen.ShowDialog() == DialogResult.OK) {
+				FileStream fileScr = new FileStream(dlgOpen.FileName, FileMode.Open, FileAccess.Read);
+				int lSource = (int)fileScr.Length;
+				byte[] tabBytes = new byte[lSource];
+				fileScr.Read(tabBytes, 0, lSource);
+				fileScr.Close();
+				bool isCpc = CpcSystem.CheckAmsdos(tabBytes);
+				if (isCpc) {
+					if (chkNoAmsdos.Checked)
+						WriteInfo("Amsdos header detected, but no Amsdos header will be written to destination file");
+					else
+						WriteInfo("Amsdos header detected, will write Amsdos header to destination file");
+
+					Array.Copy(tabBytes, 128, tabBytes, 0, lSource - 128);
+					lSource -= 128;
+				}
+				WriteInfo("Source file size:" + lSource + " (#" + lSource.ToString("X4") + ")");
+				PackMethode pkMethode = PackMethode.None;
+				switch (comboPackMethode.SelectedItem.ToString()) {
+					case "Standard":
+						pkMethode = PackMethode.Standard;
+						break;
+
+					case "ZX0":
+						pkMethode = PackMethode.ZX0;
+						break;
+
+					case "ZX1":
+						pkMethode = PackMethode.ZX1;
+						break;
+				}
+				PackModule p = new PackModule();
+				int lDest = p.Pack(tabBytes, lSource, result, 0, pkMethode);
+				WriteInfo("output file size:" + lDest + " (#" + lDest.ToString("X4") + ") (gain=" + (100 - (100 * lDest / lSource)).ToString("00") + "%)");
+				SaveFileDialog dlgSave = new SaveFileDialog();
+				dlgSave.Filter = "All files (*.*)|*.*";
+				if (dlgSave.ShowDialog() == DialogResult.OK) {
+					FileStream fs = new FileStream(dlgSave.FileName, FileMode.Create, FileAccess.ReadWrite);
+					BinaryWriter bw = new BinaryWriter(fs);
+					if (isCpc && !chkNoAmsdos.Checked) {
+						string name = Path.GetFileName(dlgSave.FileName);
+						bw.Write(CpcSystem.AmsdosToByte(CpcSystem.CreeEntete(name, 0x4000, (short)lDest, 0)));
+					}
+					bw.Write(result, 0, lDest);
+					bw.Close();
+				}
+			}
+		}
+	}
+}
